9aa309543aac92e319843a63d539287e78dd1aeb
IsabelaDomingues <32103690+IsabelaDomingues@users.noreply.github.com>
2020-09-12 11:39:44 -0300

    Add files via upload

---
+++ Smartudy/js/script.js
@@ -0,0 +1,35 @@
+//Abre menu mobile
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+});
+
+// Carrosel de imagens página inicial
+$('.carousel.carousel-slider').carousel({
+	fullWidth: true,
+	indicators: true
+});
+
+//Verifica se é mobile
+function isMobile() {
+	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+//Se não é mobile passa imagens automaticamente
+if (!isMobile()) {
+	$('.carousel').carousel({
+		padding: 200    
+	});
+	autoplay();
+	function autoplay() {
+		$('.carousel').carousel('next');
+		setTimeout(autoplay, 8000);
+	} 
+}
+
+//Accordion
+$(document).ready(function(){
+	$('.collapsible').collapsible();
+});
+
+//Menu lateral mobile
+ $(".button-collapse").sideNav();
